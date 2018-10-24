4387f5dfbafe33e8f6313fdbca54fee886e2dc06
aclarke9691 <aclarke9691@bths.edu>
2018-10-24 14:48:58 -0400

    uhh

--- src/Classroom.java
+++ src/Classroom.java
@@ -1,2 +1,14 @@
 public class Classroom {
+    private Student[] students;
+    private Teacher teacher;
+    public Classroom(String Students[], String Teacher)
+    {
+        this.teacher= teacher;
+        this.students= students;
+    }
+
+    public String getSubject()
+    {
+        String subject= subject;
+    }
 }
