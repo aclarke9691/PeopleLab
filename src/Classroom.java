50d37b0be25a6e58f972535f5d238b51f4843fdb
rchu9700 <rchu9700@bths.edu>
2018-10-24 14:50:11 -0400

    getting closer??

--- src/Classroom.java
+++ src/Classroom.java
@@ -1,2 +1,26 @@
 public class Classroom {
+    private Student[] students;
+    private Teacher teacher;
+    public Classroom(String Student[], String Teacher) {
+        this.students = students;
+        this.teacher = teacher;
+    }
+    public Student[] getStudents() {
+        return students;
+    }
+
+    public void setStudents(Student[] students) {
+        this.students = students;
+    }
+
+    public Teacher getTeacher() {
+        return teacher;
+    }
+
+    public void setTeacher(Teacher teacher) {
+        this.teacher = teacher;
+    }
+    public String getSubject() {
+        return "My subject is " +
+    }
 }
--- src/Runner.java
+++ src/Runner.java
@@ -2,6 +2,7 @@ public class Runner {
     public static void main(String[] args) {
         Student student = new Student(3.7, 274839582, "Finance", "Ricky", "Chu");
         System.out.println(student.toString());
-        Teacher teacher = new Teacher("Computer Science")
+        Teacher teacher = new Teacher("Computer Science", "Mr", "Ricky", "Chu");
+        System.out.println(teacher.toString());
     }
 }
